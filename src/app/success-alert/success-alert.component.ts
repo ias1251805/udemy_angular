e8246f7e349a51d1397dbba92e2edef42e1fb68c
Christian <christian.allan4@gmail.com>
2019-12-10 23:29:53 -0500

    Added stype

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -3,10 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
-  //styleUrls: ['./success-alert.component.css']
-  styles: [`p{
-    color: green
-}`]
+  styleUrls: ['./success-alert.component.css']
+
 })
 export class SuccessAlertComponent implements OnInit {
 
