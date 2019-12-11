8cef431576568a05d7aec4363d2966538baadbcf
Christian <christian.allan4@gmail.com>
2019-12-10 22:57:20 -0500

    Save canges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { from } from 'rxjs';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-warning-alert',
+    templateUrl: './warning-alert.html',
+    styleUrls: ['./warning-alert.css']
+})
+
+export class WarningAlertComponent implements OnInit{
+    constructor(){}
+    ngOnInit(){
+        
+    }
+}
